565b2a1f394d6a483d1c81c470e27a985b5e20d8
oalejel <alejel.omar@gmail.com>
2018-01-26 10:55:11 -0500

    1 page parsing done
    Next goal is to be able to scrape subdirectories and store in files

--- mail_scrape.py
+++ mail_scrape.py
@@ -1,5 +1,4 @@
 #! python3
-
 import sys
 from html.parser import HTMLParser
 from urllib import request
@@ -13,6 +12,7 @@ else:
 if url[0:4] != "http":
     print("Invalid input; please pass a url in the form of http(s)://________")
     exit(0)
+
 print("Site to scrape: \"" + url + "\"")
 
 # download our top level webpage
@@ -20,26 +20,52 @@ print("Site to scrape: \"" + url + "\"")
 class HTMLParser(HTMLParser):
     scrapedURLs = []
     scrapedEmails = []
+    pageUrl = ""
+    # this custom definiton will use the url to prepend to found subpaths
+    def __init__(self, url):
+        super().__init__()
+        self.pageUrl = url
+
+    def parseHref(self, h):
+        #determine if we have a new path or a new email
+        if h[0] == "/" or h[0:4] == "http":
+            # print("path: " + h)
+            self.addPathString(h)
+        else:
+            if h[0] != "#":
+                # print("email: " + h)
+                self.addEmailString(h)
+                #we can now be fairly certain that this is a umich email
+
+    def addEmailString(self, h):
+        # note that the replace method does not affect original.
+        # will return a new modified string
+        h = h.replace("mailto:", "")
+        h = h.split("?")[0]
+        self.scrapedEmails.append(h)
+
+    def addPathString(self, h):
+        if h[0] == "/":
+            h = self.pageUrl + h
+        #careful to use .append instead of += to avoid splitting the string into chars
+        self.scrapedURLs.append(h)
 
     def handle_starttag(self, tag, attrs):
-        print("encountered a start tag: " + tag)
+        # print("START: " + tag)
+        if tag == "a":
+            for att in attrs:
+                if "href" in att:
+                    #get second item in attribute pair if we have an href
+                    self.parseHref(att[1])
 
-    def handle_endtag(self, tag):
-        print("end of tag: " + tag)
+    # def handle_endtag(self, tag):
+    #     pass
 
-    def handle_data(self, data):
-        print("data: " + data)
-
-def parsePage(page):
-    for line in page.readlines():
-        print(line)
-    pass
-
-#main code
-scrapeParser = HTMLParser()
+# main code
 topPage = request.urlopen(url)
+scrapeParser = HTMLParser(url)
+scrapeParser.feed(str(topPage.read()))
 
-parsePage(topPage)
-# scrapeParser.feed(str(topPage.read()))4
-
+print(scrapeParser.scrapedEmails)
+print(scrapeParser.scrapedURLs)
 # print(str(topPage.read()))
